8e01aeae4cae097e4d305e1b12f571179fca58d9
Son Chau <son@geografia.com.au>
2020-11-19 16:12:19 +1100

    hardcode in app

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,9 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there! using: shinyinc - add invalidation E1P0QODDY87QTG</h1>
+      <h1>
+        Hi there! using: shinyinc - add invalidation E1P0QODDY87QTG- hard code
+      </h1>
       <hr />
       <MarketingApp />
     </div>
