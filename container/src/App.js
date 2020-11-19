258f5b217382c9c828892e7c2fae55d7dbdfd36e
Son Chau <son@geografia.com.au>
2020-11-19 15:58:20 +1100

    add E1P0QODDY87QTG validatiion

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there! using: shinyinc</h1>
+      <h1>Hi there! using: shinyinc - add invalidation E1P0QODDY87QTG</h1>
       <hr />
       <MarketingApp />
     </div>
