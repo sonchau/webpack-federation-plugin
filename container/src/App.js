e96b08881f2194c6d4f683d79d417091336d6c58
Son Chau <son@geografia.com.au>
2020-11-19 14:11:32 +1100

    use shinyinc

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there! using: ItsKarma/aws-cli@v1.70.0</h1>
+      <h1>Hi there! using: shinyinc</h1>
       <hr />
       <MarketingApp />
     </div>
