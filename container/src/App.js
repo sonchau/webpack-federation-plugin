b1d7b139ca08621558722f3c27395b9ba4f0bc84
Son Chau <son@geografia.com.au>
2020-11-21 09:29:55 +1100

    add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from "./components/Header";
 export default () => {
   return (
-    <div>
-      <h1>Hi there! using: shinyinc - add distribution E22M6B6XK8I10F</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
